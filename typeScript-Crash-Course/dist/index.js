3fcbe91706504c9854953f84773d2495d0d51a03
Aman Singh <amansingh1100b@gmail.com>
2025-11-17 10:33:56 +0530

    crash course done

--- typeScript-Crash-Course/dist/index.js
+++ typeScript-Crash-Course/dist/index.js
@@ -119,8 +119,277 @@ const person1 = {
     phone: "123-456-7890",
 };
 // arrays
-let num = [1, 2, 3, 4, 5, 6];
+let num = [1, 2, 3];
 for (let i = 0; i < num.length; i++) {
     console.log(`Numbers${num[i]}`);
 }
+// multi dimensional array
+let arrayOne = [1, 2, 3, 4, 5];
+let arrayTwo = ["a", "bn"];
+let arrayThree = [1, 2, 3, "b"];
+// tuples -> similar to array with fix number of elements
+let article = [11, "title", true];
+article = [12, "two", false];
+console.log(article);
+const [id, title, published] = article;
+console.log(id);
+// enums->set of names constant values
+var Days;
+(function (Days) {
+    Days[Days["Sunday"] = 0] = "Sunday";
+    Days[Days["Monday"] = 1] = "Monday";
+    Days[Days["Tuesday"] = 2] = "Tuesday";
+    Days[Days["Wednesday"] = 3] = "Wednesday";
+    Days[Days["Thursday"] = 4] = "Thursday";
+    Days[Days["Friday"] = 5] = "Friday";
+    Days[Days["Saturday"] = 6] = "Saturday";
+})(Days || (Days = {}));
+// here 0,1,2,3,4 -> sun mon tue ...
+const today = Days.Wednesday;
+console.log(`Today is ${Days[today]}`);
+const John = {
+    name: "Jhon",
+    age: 30,
+};
+const Sarah = {
+    name: "Sarah",
+    age: 30,
+    greet(message) {
+        console.log(`${this.name} says ${message}`);
+    },
+};
+Sarah.greet("Hello, TypeScript");
+let userSetting = {
+    theme: true,
+    font: "Open Sans",
+    sidebar: false,
+    external: true,
+};
+class Labrador {
+    type = "dog";
+    bark() {
+        console.log("woof !");
+    }
+}
+const labrador = {
+    type: "monkey",
+    bark() {
+        console.log("woof");
+    },
+};
+// -> cant be extended,used to define union instructions
+// class-> blueprint for creating object with shared method and properties
+class Product {
+    id;
+    name;
+    price;
+    constructor(id, name, price) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+    }
+    getProductInfo() {
+        return `ID: ${this.id}, Name: ${this.name}, Price: ${this.price}`;
+    }
+}
+const product1 = new Product(1, "wigit", 20.7);
+console.log(product1.getProductInfo());
+// class access modifiers
+// -> public (default)
+class MyClassPublic {
+    name;
+    constructor(name) {
+        this.name = name;
+    }
+}
+const instancePublic = new MyClassPublic("Joo");
+console.log(instancePublic.name);
+// ->private
+class MyClassPrivate {
+    secret;
+    constructor(secret) {
+        this.secret = secret;
+    }
+    revealSecret() {
+        console.log(this.secret);
+    }
+}
+const instancePrivate = new MyClassPrivate("My secret");
+// console.log(instancePrivate.secret);
+instancePrivate.revealSecret();
+// protected (accessible from within the class or subclass)
+class Parent {
+    familyName;
+    constructor(name) {
+        this.familyName = name;
+    }
+}
+class Child extends Parent {
+    introduceFamily() {
+        console.log(`Our family name is ${this.familyName}`);
+    }
+}
+const parentEx = new Parent("Smith");
+const childEx = new Child("johnson");
+// console.log(parent.familyName);
+childEx.introduceFamily();
+// class accessor (getter setter)
+class ProductExAccessor {
+    id;
+    name;
+    _price;
+    constructor(id, name) {
+        this.id = id;
+        this.name = name;
+        this._price = 0;
+        // default value
+    }
+    // public getter for price
+    get price() {
+        return this._price;
+    }
+    // public setter
+    set price(newPrice) {
+        if (newPrice >= 0) {
+            this._price = newPrice;
+        }
+        else {
+            console.log(`Price cannot be negative`);
+        }
+    }
+    getProductInfo() {
+        return `ID: ${this.id} ,Price: ${this._price}`;
+    }
+}
+const productEx = new ProductExAccessor(1, "wig");
+console.log(productEx.getProductInfo());
+// this is like calling setter function
+productEx.price = 20.0;
+console.log(productEx.getProductInfo());
+productEx.price = -5;
+// class static members (properties and methods belongs to class itself rather then instances-> can  be used without instance of class)
+class ProductStaticMembers {
+    id;
+    name;
+    static nextId = 1;
+    constructor(id, name) {
+        this.id = id;
+        this.name = name;
+    }
+    static generateNextId() {
+        return ProductStaticMembers.nextId++;
+    }
+    getProductInfo() {
+        return `ID: ${this.id} Name: ${this.name}`;
+    }
+}
+const product1static = new ProductStaticMembers(ProductStaticMembers.generateNextId(), "Widget");
+const product2static = new ProductStaticMembers(ProductStaticMembers.generateNextId(), "Gad");
+console.log(product1static.getProductInfo());
+console.log(product2static.getProductInfo());
+/**********************************
+ *class implement interface
+ **************************************/
+class ProductImplementInterface {
+    id;
+    name;
+    static nextId = 1;
+    // shorthand of the bigger this.id and all
+    constructor(id, name) {
+        this.id = id;
+        this.name = name;
+    }
+    static generateNextId() {
+        return ProductImplementInterface.nextId++;
+    }
+    getProductInfo() {
+        return `ID: ${this.id} Name: ${this.name}`;
+    }
+}
+const product1implement = new ProductImplementInterface(ProductImplementInterface.generateNextId(), "wid");
+const product2implement = new ProductImplementInterface(ProductImplementInterface.generateNextId(), "loo");
+console.log(product1implement.getProductInfo());
+console.log(product2implement.getProductInfo());
+/*****************************************
+ abstract classes and members (blueprint of other class cant be instanceated on there own also has abstract methods which are declaration but not implemented need subclass to implement)
+ ******************************************/
+class AbstractItem {
+    id;
+    name;
+    static nextID = 1;
+    constructor(id, name) {
+        this.id = id;
+        this.name = name;
+    }
+    static generateNextId() {
+        return AbstractItem.nextID++;
+    }
+}
+class Item extends AbstractItem {
+    constructor(id, name) {
+        super(id, name);
+    }
+    getItemInfo() {
+        return `ID: ${this.id}, Name:  ${this.name}`;
+    }
+}
+const item1 = new Item(AbstractItem.generateNextId(), "heh");
+console.log(item1.getItemInfo());
+/**************************************
+polymorphism and methods override (object of different classes treated as object of single classes)
+ *******************************************/
+// define abstract class
+class AbstractEntity {
+    id;
+    name;
+    static nextId = 1;
+    constructor(id, name) {
+        this.id = id;
+        this.name = name;
+    }
+    static generateNextId() {
+        return AbstractEntity.nextId++;
+    }
+}
+// create a concrete subclass
+class Entity extends AbstractEntity {
+    constructor(id, name) {
+        super(id, name);
+    }
+    getEntityInfo() {
+        return `ID: ${this.id} Name: ${this.name}`;
+    }
+}
+// create another concrete subclass
+class AnotherEntity extends AbstractEntity {
+    constructor(id, name) {
+        super(id, name);
+    }
+    getEntityInfo() {
+        return `ID: ${this.id} Name: ${this.name} Additional Info...`;
+    }
+}
+// generate unique IDs for entity using the static method
+const entity1 = new Entity(AbstractEntity.generateNextId(), "wig");
+const entity2 = new AnotherEntity(AbstractEntity.generateNextId(), "God");
+// polymorphism call
+console.log(entity1.getEntityInfo());
+console.log(entity2.getEntityInfo());
+/**
+ * class is creating object
+ * interface used to define the shape of object
+ *
+ */
+/**
+ * GENERICS
+ * allow t write reusable code by passing type as a parameter to another type weather its class interface or function
+ */
+function returnType(val) {
+    return val;
+}
+const numValue = returnType(100);
+const strValue = returnType("heh");
+const boolValue = returnType(true);
+const arrValue = returnType([1, 2, 3, 4]);
+console.log(`Array Value: ${arrValue}`);
 //# sourceMappingURL=index.js.map
--- typeScript-Crash-Course/src/index.ts
+++ typeScript-Crash-Course/src/index.ts
@@ -184,3 +184,367 @@ let arrayTwo: string[] = ["a", "bn"];
 let arrayThree: (string | number)[] = [1, 2, 3, "b"];
 
 // tuples -> similar to array with fix number of elements
+
+let article: readonly [number, string, boolean] = [11, "title", true];
+article = [12, "two", false];
+console.log(article);
+
+const [id, title, published] = article;
+console.log(id);
+
+// enums->set of names constant values
+
+enum Days {
+  Sunday,
+  Monday,
+  Tuesday,
+  Wednesday,
+  Thursday,
+  Friday,
+  Saturday,
+}
+// here 0,1,2,3,4 -> sun mon tue ...
+
+const today: Days = Days.Wednesday;
+console.log(`Today is ${Days[today]}`);
+
+// interface-> define shape and structure of object->specifies property, method and its type
+/** *
+ * interface specified object structure including property names types and optional/required status.
+ * contracts->object or classes adhering to an interface must implement its defined structure and methods
+ * type checking-> typescript checks if object meet the interface's required catching type error early
+ * code clarity-> interface document expected object properties and enhance code readability,aiding object usage understanding
+ *
+ */
+
+interface PersonEx1 {
+  name: string;
+  age: number;
+}
+
+const John: PersonEx1 = {
+  name: "Jhon",
+  age: 30,
+};
+
+// interface method and parameter
+
+interface PersonEx2 {
+  name: string;
+  age: number;
+  greet(message: string): void;
+}
+
+const Sarah: PersonEx2 = {
+  name: "Sarah",
+  age: 30,
+  greet(message: string) {
+    console.log(`${this.name} says ${message}`);
+  },
+};
+
+Sarah.greet("Hello, TypeScript");
+
+// reopen interface and used cases-> expanding interface
+
+// Homepage interface declaration
+interface Setting {
+  readonly theme: boolean;
+  font: string;
+}
+
+// article page interface reopening
+interface Setting {
+  sidebar: boolean;
+}
+
+// contact page interface
+interface Setting {
+  external: boolean;
+}
+
+let userSetting: Setting = {
+  theme: true,
+  font: "Open Sans",
+  sidebar: false,
+  external: true,
+};
+
+// used cases
+// -> modular development
+// -> progressive enhancement
+// -> clear separation of concerns
+
+// built-in interface
+
+// const imgElement: HTMLImageElement = document.createElement("img");
+// imgElement.src = "img.jpg";
+// imgElement.alt = "My Image";
+
+// interface vs Type alias
+// interface
+interface AnimalExInterface {
+  type: string;
+}
+interface Dog extends AnimalExInterface {
+  bark(): void;
+}
+class Labrador implements Dog {
+  type: string = "dog";
+  bark() {
+    console.log("woof !");
+  }
+}
+// -> can be extended,compatible,extensibility.readability and style,
+
+// type
+type AnimalExType = {
+  type: string;
+};
+type Monkey = AnimalExInterface & {
+  bark(): void;
+};
+const labrador: Monkey = {
+  type: "monkey",
+  bark() {
+    console.log("woof");
+  },
+};
+// -> cant be extended,used to define union instructions
+
+// class-> blueprint for creating object with shared method and properties
+
+class Product {
+  id: number;
+  name: string;
+  price: number;
+
+  constructor(id: number, name: string, price: number) {
+    this.id = id;
+    this.name = name;
+    this.price = price;
+  }
+
+  getProductInfo(): string {
+    return `ID: ${this.id}, Name: ${this.name}, Price: ${this.price}`;
+  }
+}
+const product1 = new Product(1, "wigit", 20.7);
+console.log(product1.getProductInfo());
+
+// class access modifiers
+
+// -> public (default)
+class MyClassPublic {
+  public name: string;
+  constructor(name: string) {
+    this.name = name;
+  }
+}
+const instancePublic = new MyClassPublic("Joo");
+console.log(instancePublic.name);
+
+// ->private
+class MyClassPrivate {
+  private secret: string;
+  constructor(secret: string) {
+    this.secret = secret;
+  }
+  revealSecret() {
+    console.log(this.secret);
+  }
+}
+const instancePrivate = new MyClassPrivate("My secret");
+// console.log(instancePrivate.secret);
+instancePrivate.revealSecret();
+
+// protected (accessible from within the class or subclass)
+class Parent {
+  protected familyName: string;
+  constructor(name: string) {
+    this.familyName = name;
+  }
+}
+class Child extends Parent {
+  introduceFamily() {
+    console.log(`Our family name is ${this.familyName}`);
+  }
+}
+
+const parentEx = new Parent("Smith");
+const childEx = new Child("johnson");
+
+// console.log(parent.familyName);
+childEx.introduceFamily();
+
+// class accessor (getter setter)
+
+class ProductExAccessor {
+  private _price: number;
+  constructor(private id: number, private name: string) {
+    this._price = 0;
+    // default value
+  }
+  // public getter for price
+  get price(): number {
+    return this._price;
+  }
+  // public setter
+  set price(newPrice: number) {
+    if (newPrice >= 0) {
+      this._price = newPrice;
+    } else {
+      console.log(`Price cannot be negative`);
+    }
+  }
+  getProductInfo(): string {
+    return `ID: ${this.id} ,Price: ${this._price}`;
+  }
+}
+const productEx = new ProductExAccessor(1, "wig");
+console.log(productEx.getProductInfo());
+// this is like calling setter function
+productEx.price = 20.0;
+console.log(productEx.getProductInfo());
+productEx.price = -5;
+
+// class static members (properties and methods belongs to class itself rather then instances-> can  be used without instance of class)
+class ProductStaticMembers {
+  private static nextId: number = 1;
+  constructor(private id: number, private name: string) {}
+  static generateNextId(): number {
+    return ProductStaticMembers.nextId++;
+  }
+  getProductInfo(): string {
+    return `ID: ${this.id} Name: ${this.name}`;
+  }
+}
+const product1static = new ProductStaticMembers(
+  ProductStaticMembers.generateNextId(),
+  "Widget"
+);
+const product2static = new ProductStaticMembers(
+  ProductStaticMembers.generateNextId(),
+  "Gad"
+);
+
+console.log(product1static.getProductInfo());
+console.log(product2static.getProductInfo());
+
+/**********************************
+ *class implement interface
+ **************************************/
+class ProductImplementInterface {
+  private static nextId: number = 1;
+  // shorthand of the bigger this.id and all
+  constructor(private id: number, private name: string) {}
+  static generateNextId(): number {
+    return ProductImplementInterface.nextId++;
+  }
+  getProductInfo(): string {
+    return `ID: ${this.id} Name: ${this.name}`;
+  }
+}
+const product1implement = new ProductImplementInterface(
+  ProductImplementInterface.generateNextId(),
+  "wid"
+);
+const product2implement = new ProductImplementInterface(
+  ProductImplementInterface.generateNextId(),
+  "loo"
+);
+
+console.log(product1implement.getProductInfo());
+console.log(product2implement.getProductInfo());
+
+/*****************************************
+ abstract classes and members (blueprint of other class cant be instanceated on there own also has abstract methods which are declaration but not implemented need subclass to implement)
+ ******************************************/
+
+abstract class AbstractItem {
+  private static nextID: number = 1;
+  constructor(public id: number, protected name: string) {}
+  static generateNextId(): number {
+    return AbstractItem.nextID++;
+  }
+  abstract getItemInfo(): string;
+}
+
+class Item extends AbstractItem {
+  constructor(id: number, name: string) {
+    super(id, name);
+  }
+  getItemInfo(): string {
+    return `ID: ${this.id}, Name:  ${this.name}`;
+  }
+}
+
+const item1 = new Item(AbstractItem.generateNextId(), "heh");
+console.log(item1.getItemInfo());
+
+/**************************************
+polymorphism and methods override (object of different classes treated as object of single classes)
+ *******************************************/
+
+// define abstract class
+abstract class AbstractEntity {
+  private static nextId: number = 1;
+  protected constructor(public id: number, protected name: string) {}
+  static generateNextId(): number {
+    return AbstractEntity.nextId++;
+  }
+  abstract getEntityInfo(): string;
+}
+// create a concrete subclass
+class Entity extends AbstractEntity {
+  constructor(id: number, name: string) {
+    super(id, name);
+  }
+  getEntityInfo(): string {
+    return `ID: ${this.id} Name: ${this.name}`;
+  }
+}
+
+// create another concrete subclass
+class AnotherEntity extends AbstractEntity {
+  constructor(id: number, name: string) {
+    super(id, name);
+  }
+  getEntityInfo(): string {
+    return `ID: ${this.id} Name: ${this.name} Additional Info...`;
+  }
+}
+// generate unique IDs for entity using the static method
+const entity1: AbstractEntity = new Entity(
+  AbstractEntity.generateNextId(),
+  "wig"
+);
+const entity2: AbstractEntity = new AnotherEntity(
+  AbstractEntity.generateNextId(),
+  "God"
+);
+
+// polymorphism call
+console.log(entity1.getEntityInfo());
+console.log(entity2.getEntityInfo());
+
+/**
+ * class is creating object
+ * interface used to define the shape of object
+ *
+ */
+
+/**
+ * GENERICS
+ * allow t write reusable code by passing type as a parameter to another type weather its class interface or function
+ */
+function returnType<T>(val: T): T {
+  return val;
+}
+
+const numValue: number = returnType<number>(100);
+const strValue: string = returnType<string>("heh");
+const boolValue: boolean = returnType<boolean>(true);
+const arrValue: number[] = returnType<number[]>([1, 2, 3, 4]);
+
+console.log(`Array Value: ${arrValue}`);
